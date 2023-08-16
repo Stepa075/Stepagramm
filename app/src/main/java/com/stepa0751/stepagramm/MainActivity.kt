1d16d4eb4f5cfdf1777ab37a9671740d97974107
Stepa075 <stepa0751@gmail.com>
2023-08-16 20:04:13 +0300

    Initial commit

---
+++ app/src/main/java/com/stepa0751/stepagramm/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.stepa0751.stepagramm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
